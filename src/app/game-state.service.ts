4254978ac879a02d01550bba7acd7ffa9b9f9cc6
Xirema <xiremaxesirin@gmail.com>
2024-01-14 23:57:31 -0600

    Using Linux Server for Game States

--- src/app/game-state.service.ts
+++ src/app/game-state.service.ts
@@ -14,22 +14,22 @@ export class GameStateService {
   constructor(private httpClient:HttpClient) {}
 
   public getGameState(gameId:string):Observable<GameState> {
-    return this.httpClient.post<GameState>("https://localhost:8167/state/getGameState", {gameId:gameId});
+    return this.httpClient.post<GameState>("https://xserver:8167/state/getGameState", {gameId:gameId});
   }
 
   public getUnitStates(gameId:string):Observable<UnitState[]> {
-    return this.httpClient.post<UnitState[]>("https://localhost:8167/state/getUnitStates", {gameId:gameId});
+    return this.httpClient.post<UnitState[]>("https://xserver:8167/state/getUnitStates", {gameId:gameId});
   }
 
   public getTerrainStates(gameId:string):Observable<TerrainState[]> {
-    return this.httpClient.post<TerrainState[]>("https://localhost:8167/state/getTerrainStates", {gameId:gameId});
+    return this.httpClient.post<TerrainState[]>("https://xserver:8167/state/getTerrainStates", {gameId:gameId});
   }
 
   public getPlayerStates(gameId:string):Observable<PlayerState[]> {
-    return this.httpClient.post<PlayerState[]>("https://localhost:8167/state/getPlayerStates", {gameId:gameId});
+    return this.httpClient.post<PlayerState[]>("https://xserver:8167/state/getPlayerStates", {gameId:gameId});
   }
 
   public getSettingState(gameId:string):Observable<SettingsState> {
-    return this.httpClient.post<SettingsState>("https://localhost:8167/state/getSettingState", {gameId:gameId});
+    return this.httpClient.post<SettingsState>("https://xserver:8167/state/getSettingState", {gameId:gameId});
   }
 }
